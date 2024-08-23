4ad9458435950a6f2fbfca56a6247e237885505d
keriko <13868755+keriko@user.noreply.gitee.com>
2024-08-23 15:31:30 +0800

    粘包问题测试

---
+++ echo-rpc-core/src/main/java/com/keriko/echorpc/server/tcp/parser/VertxTcpClientParser.java
@@ -0,0 +1,41 @@
+package com.keriko.echorpc.server.tcp.parser;
+
+
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class VertxTcpClientParser {
+
+    public void start() {
+        // 创建 Vert.x 实例
+        Vertx vertx = Vertx.vertx();
+
+        vertx.createNetClient().connect(8888, "localhost", result -> {
+            if (result.succeeded()) {
+                log.info("Connected to TCP server,{}:{}","localhost", 8888);
+                io.vertx.core.net.NetSocket socket = result.result();
+                for (int i = 0; i < 1000; i++) {
+                    // 发送数据
+                    Buffer buffer = Buffer.buffer();
+                    String str = "Hello, server!Hello, server!Hello, server!Hello, server!";
+                    buffer.appendInt(0);
+                    buffer.appendInt(str.getBytes().length);
+                    buffer.appendBytes(str.getBytes());
+                    socket.write(buffer);
+                }
+                // 接收响应
+                socket.handler(buffer -> {
+                    System.out.println("Received response from server: " + buffer.toString());
+                });
+            } else {
+                System.err.println("Failed to connect to TCP server");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new VertxTcpClientParser().start();
+    }
+}
